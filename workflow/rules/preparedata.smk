b68d6161f0f0b00d079dd917bc0bdb8476dbdf71
Ann-Kathrin Brueggemann <ann-kathrin.brueggemann@uk-essen.de>
2022-03-16 10:28:53 +0100

    fmt

--- workflow/rules/preparedata.smk
+++ workflow/rules/preparedata.smk
@@ -1,12 +1,11 @@
 rule data_prep:
     input:
-        config["metadata"]
+        config["metadata"],
     output:
-        metadata = "config/pep/sample.tsv",
-        sample_info = "config/pep/sample_info.txt",
-    priority:
-        50
+        metadata="config/pep/sample.tsv",
+        sample_info="config/pep/sample_info.txt",
+    priority: 50
     log:
-        "logs/data_prep.txt"
+        "logs/data_prep.txt",
     script:
         "../scripts/create_sample_metadata.py"
--- workflow/scripts/relative_abundance.py
+++ workflow/scripts/relative_abundance.py
@@ -3,8 +3,9 @@ import gzip
 import shutil
 import os
 import zipfile
+
 # Extracting the number of total features over all samples from the sample-table.
-# Multiplying the number with the relative abundance filtering value to create a threshold for the 
+# Multiplying the number with the relative abundance filtering value to create a threshold for the
 # qiime filtering.
 
 # Reading the sample-table, creating a zip file
@@ -13,11 +14,11 @@ name = os.path.splitext(file)[0]
 shutil.copy(file, name + ".zip")
 filename = name + ".zip"
 # Extract zip files to folder
-with zipfile.ZipFile(filename,"r") as zip_ref:
+with zipfile.ZipFile(filename, "r") as zip_ref:
     name = filename.split("/")[-1]
     dir_name = os.path.dirname(str(snakemake.input))
     new_dir = dir_name + "/" + name
-    zip_ref.extractall(os.path.splitext(new_dir)[0]+"/")
+    zip_ref.extractall(os.path.splitext(new_dir)[0] + "/")
 name = name.split(".")[0]
 directory = os.path.dirname(str(snakemake.input)) + "/" + name
 # Moving the folder inventory one folder up
@@ -31,9 +32,9 @@ while b < len(subdir):
         shutil.move(path, new_dir)
     b = b + 1
 # Read the specific csv holding the information, creating a dataframe, adding up all feature frequencies
-datadir = directory +"/"+ "data/"
+datadir = directory + "/" + "data/"
 csv = datadir + "sample-frequency-detail.csv"
-frequency = pd.read_csv(csv, header = 0, delimiter = ",")
+frequency = pd.read_csv(csv, header=0, delimiter=",")
 number = frequency["0"].sum()
 # Creating the abundance threshold and storing it in an output file
 abundance = float(str(snakemake.params))
@@ -41,6 +42,3 @@ endnumber = number * abundance
 endnumber = int(endnumber)
 with open(str(snakemake.output), "w") as f:
     f.write(str(endnumber))
-
-
-        
